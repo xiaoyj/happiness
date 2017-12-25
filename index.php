dae992357e89cf5b99b1669326143dda17b3edb0
xiaoyijie <xiaoyijie2007@126.com>
2017-12-25 15:43:35 +0800

    init my project

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+echo '<script>location.href="wall/index.php?style=jiuba";</script>';
+?>
---
+++ wall/index.php
@@ -0,0 +1,20 @@
+<?php 
+@header("Content-type: text/html; charset=utf-8");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>婚礼上墙大屏幕</title>
+<script type="text/javascript"> 
+if(document.all){
+	alert("ie浏览器无法正常解析本页，请使用谷歌内核的流量器浏览。如（360浏览器，猎豹浏览器等）");
+	window.history.back(-1); 
+	}
+</script> 
+</head>
+
+<body>
+	
+</body>
+</html>
