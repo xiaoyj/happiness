493a9d4cd0bdfafd9d5ad37280772caff836b088
xiaoyijie <xiaoyijie2007@126.com>
2017-12-25 15:48:50 +0800

    init the index.php

--- wall/index.php
+++ wall/index.php
@@ -15,6 +15,63 @@ if(document.all){
 </head>
 
 <body>
+	<div class="main">
+   <EMBED style=" z-index:-2;RIGHT: 250px; POSITION: absolute; TOP: 55px; absolute: " align=right src=".style/<?php echo $style?>/images/fl.swf" width=1600 height=625 type=application/x-shockwave-flash wmode="transparent" quality="high" ;></EMBED>
+   <EMBED style="z-index:-2;LEFT: 250px; POSITION: absolute; TOP: 55px; absolute: " align=right src=".style/<?php echo $style?>/images/fl.swf" width=1600 height=625 type=application/x-shockwave-flash wmode="transparent" quality="high" ;></EMBED>
+
+<div class="l"></div>
+<div class="r"></div>
+<div class="top" onClick="viewExplan();" data-position="right center" data-content="二维码，快捷键M">
+	<?php 
+		$i=1;
+		for(;$i<20;$i++){
+			if(file_exists('logo/'.$i.'.png')){
+			}else{
+				break;
+			}
+		}
+		if ($i <= 2){
+			echo '<div class="top-logo">';
+		}else{
+			echo '<div class="ui shape top-logo">';
+		}
 	
+	?>
+	<div class="sides">
+	<?php 
+		for($i=1;$i<20;$i++){
+			if(file_exists('logo/'.$i.'.png')){
+				if($i==1){
+					echo '<img src="logo/1.png" width=455px height=135px class="active side"/>';
+				}else{
+					echo '<img src="logo/'.$i.'.png" width=455px height=135px class="side"/>';
+				}
+			}
+		}
+	
+	?>
+	</div>
+
+</div>
+<div class="kword ui shape ">
+	<div class="sides">
+		<div class="k active side">关注我们的官方账号<strong><?php echo $dianplu_wxh;?></strong> <br>发送<?php  echo $xuanzezu[0];?>您想说的话即可上墙！</div>
+
+		<div class="k side"><?php  echo $xuanzezu[1];?></div>
+
+		<div class="k side"><?php  echo $xuanzezu[2];?></div>
+	</div>
+</div>
+</div>
+    <div class="wall">
+        <div class="left">
+     
+        </div>
+      <div class="center">
+            <div class="list">
+                <ul id="list"></ul>
+            </div>
+            <div class="footer"></div>
+        <div class="btns">
 </body>
 </html>
